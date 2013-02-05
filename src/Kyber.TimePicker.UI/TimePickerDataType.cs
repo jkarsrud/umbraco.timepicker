dbac30fbb99a5d83dcfc2c6139208711bf5c7a64
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2013-02-05 22:52:27 +0100

    Started the project of making an Umbraco Timepicker datatype

---
+++ src/Kyber.TimePicker.UI/TimePickerDataType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.cms.businesslogic.datatype;
+
+namespace Kyber.TimePicker.UI
+{
+    public class TimePickerDataType : AbstractDataEditor
+    {
+
+
+        public override string DataTypeName
+        {
+            get { return "Umbraco TimePicker"; }
+        }
+
+        public override Guid Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
