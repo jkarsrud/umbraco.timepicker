6c8681475bd03a8665e720170756380f491ac5e8
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2013-02-10 20:47:37 +0100

    Renamed from Kyber to JKarsrud for now

--- src/JKarsrud.TimePicker.UI/TimePickerDataType.cs
+++ src/JKarsrud.TimePicker.UI/TimePickerDataType.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using umbraco.cms.businesslogic.datatype;
 
-namespace Kyber.TimePicker.UI
+namespace JKarsrud.TimePicker.UI
 {
     public class TimePickerDataType : AbstractDataEditor
     {
-
-
         public override string DataTypeName
         {
             get { return "Umbraco TimePicker"; }
